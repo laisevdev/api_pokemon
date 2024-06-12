ab5c304c514ebd36685e8d24f739aa90762a027c
Laíse Vieira <laisevdev@gmail.com>
2024-06-12 00:26:48 -0300

    [feat]: add admins

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pokemon, Capacidades, Deslocamento, GolpesNaturais, AtributosBasais, TiposHabilidades, Comportamento, Narrador, Medidas, Procriacao
 
-# Register your models here.
+admin.site.register(Pokemon)
+admin.site.register(Capacidades)
+admin.site.register(Deslocamento)
+admin.site.register(GolpesNaturais)
+admin.site.register(AtributosBasais)
+admin.site.register(TiposHabilidades)
+admin.site.register(Comportamento)
+admin.site.register(Narrador)
+admin.site.register(Medidas)
+admin.site.register(Procriacao)
+
+
