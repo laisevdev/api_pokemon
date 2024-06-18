8b159730ba52599b06230cef7b6d47537daafad2
Laíse Vieira <laisevdev@gmail.com>
2024-06-18 15:15:59 -0300

    [feat]: view Pokemon

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Pokemon
+#Capacidades, Comportamento, Deslocamento, GolpesNaturais, AtributosBasais, TiposHabilidades, Narrador, Medidas, Procriacao 
+from .serializers import PokemonSerializer
+#CapacidadesSerializer, ComportamentoSerializer, DeslocamentoSerializer, GolpesNaturaisSerializer, AtributosBasaisSerializer, TiposHabilidadesSerializer, NarradorSerializer, MedidasSerializer, ProcriacaoSerializer
 
-# Create your views here.
+class PokemonAPIView(APIView):
+    def get(self, request): 
+        pokemons = Pokemon.objects.all()
+        serializer = PokemonSerializer(pokemons, many=True)
+        return Response(serializer.data)
