441a053b6830217f4c7a2e72d9645540345ef003
Laíse Vieira <laisevdev@gmail.com>
2024-06-19 17:15:40 -0300

    [refactor]: add upload to on image

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     nome_pokemon = models.CharField(max_length=200)
-    imagem = models.ImageField()
+    imagem = models.ImageField(upload_to='imgs/')
 
 
 class Capacidades(models.Model):
