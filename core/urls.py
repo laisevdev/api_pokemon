e0838bb301965c77c2b64913b9782d6aa7963341
Laíse Vieira <laisevdev@gmail.com>
2024-06-20 11:03:12 -0300

    [feat]: add urls path

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,20 @@
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import PokemonAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PokemonViewSet, CapacidadesViewSet, ComportamentoViewSet, TiposHabilidadesViewSet, DeslocamentoViewSet, GolpesNaturaisViewSet, AtributosBasaisViewSet, NarradorViewSet, MedidasViewSet, ProcriacaoViewSet
+
+router = DefaultRouter()
+router.register(r'pokemons', PokemonViewSet)
+router.register(r'capacidades', CapacidadesViewSet)
+router.register(r'comportamentos', ComportamentoViewSet)
+router.register(r'tiposhabilidades', TiposHabilidadesViewSet)
+router.register(r'deslocamentos', DeslocamentoViewSet)
+router.register(r'golpes', GolpesNaturaisViewSet)
+router.register(r'atributos', AtributosBasaisViewSet)
+router.register(r'narrador', NarradorViewSet)
+router.register(r'medidas', MedidasViewSet)
+router.register(r'procriacao', ProcriacaoViewSet)
 
 urlpatterns = [
-    path('pokemons/', PokemonAPIView.as_view(), name='pokemons')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include(router.urls)),
+]
+
