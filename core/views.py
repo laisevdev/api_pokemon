84e84ca1c4812fc50379bee8fc658c9218c2b884
Laíse Vieira <laisevdev@gmail.com>
2024-06-20 11:04:12 -0300

    [feat]: add all viewSets project

--- core/views.py
+++ core/views.py
@@ -1,13 +1,74 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Pokemon
-#Capacidades, Comportamento, Deslocamento, GolpesNaturais, AtributosBasais, TiposHabilidades, Narrador, Medidas, Procriacao 
-from .serializers import PokemonSerializer
-#CapacidadesSerializer, ComportamentoSerializer, DeslocamentoSerializer, GolpesNaturaisSerializer, AtributosBasaisSerializer, TiposHabilidadesSerializer, NarradorSerializer, MedidasSerializer, ProcriacaoSerializer
+from rest_framework import viewsets
+from .models import Pokemon, Capacidades, Comportamento, TiposHabilidades, Deslocamento, GolpesNaturais, AtributosBasais, Narrador, Medidas, Procriacao
+from .serializers import PokemonSerializer, CapacidadesSerializer, ComportamentoSerializer, TiposHabilidadesSerializer, DeslocamentoSerializer, GolpesNaturaisSerializer, AtributosBasaisSerializer , NarradorSerializer, MedidasSerializer, ProcriacaoSerializer
 
-class PokemonAPIView(APIView):
-    def get(self, request): 
-        pokemons = Pokemon.objects.all()
-        serializer = PokemonSerializer(pokemons, many=True)
-        return Response(serializer.data)
+class PokemonViewSet(viewsets.ModelViewSet):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class CapacidadesViewSet(viewsets.ModelViewSet):
+    queryset = Capacidades.objects.all()
+    serializer_class = CapacidadesSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class ComportamentoViewSet(viewsets.ModelViewSet):
+    queryset = Comportamento.objects.all()
+    serializer_class = ComportamentoSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+#RESOLVER PROBLEM    
+class TiposHabilidadesViewSet(viewsets.ModelViewSet):
+    queryset = TiposHabilidades.objects.all()
+    serializer_class = TiposHabilidadesSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class DeslocamentoViewSet(viewsets.ModelViewSet):
+    queryset = Deslocamento.objects.all()
+    serializer_class = DeslocamentoSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class GolpesNaturaisViewSet(viewsets.ModelViewSet):
+    queryset = GolpesNaturais.objects.all()
+    serializer_class = GolpesNaturaisSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class AtributosBasaisViewSet(viewsets.ModelViewSet):
+    queryset = AtributosBasais.objects.all()
+    serializer_class = AtributosBasaisSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class NarradorViewSet(viewsets.ModelViewSet):
+    queryset = Narrador.objects.all()
+    serializer_class = NarradorSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class MedidasViewSet(viewsets.ModelViewSet):
+    queryset = Medidas.objects.all()
+    serializer_class = MedidasSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
+class ProcriacaoViewSet(viewsets.ModelViewSet):
+    queryset = Procriacao.objects.all()
+    serializer_class = ProcriacaoSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
