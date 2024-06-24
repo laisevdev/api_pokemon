f18e95c3b6c5c81811c2fadffac13aada30e1ec1
Laíse Vieira <laisevdev@gmail.com>
2024-06-23 22:04:59 -0300

    [refactor]: att routers

--- api_pokemon/urls.py
+++ api_pokemon/urls.py
@@ -1,3 +1,4 @@
+'''
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +7,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls'))
 ]
+'''
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls), 
+    path('api/v1/', include('core.urls')),
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL_2, document_root=settings.MEDIA_ROOT_2)
