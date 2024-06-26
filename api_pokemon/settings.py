139dd0599a39838e48da10630583fc63fc8a11d2
Laíse Vieira <laisevdev@gmail.com>
2024-06-26 10:30:19 -0300

    [feat]: add corsheaders on project

--- api_pokemon/settings.py
+++ api_pokemon/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'api_pokemon.urls'
